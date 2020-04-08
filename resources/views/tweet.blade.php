3e12e0275667df10cc67952d72f8d5bf48107229
Luis <pgd_online@hotmail.com>
2020-04-07 21:29:47 -0300

    Adicionado bootstrap ao projeto / SCSS

--- resources/views/tweet.blade.php
+++ resources/views/tweet.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title></title>
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     </head>
     <body>
         <form class="" action="" method="post">
@@ -12,7 +13,9 @@
             <label for="tweet">
                 <textarea name="tweet" rows="8" cols="80"></textarea>
             </label>
-            <button type="submit" name="button">Tweetar</button>
+            <button type="submit" name="button" class="btn btn-success">Tweetar</button>
         </form>
+        <script src="{{ asset('js/jquery.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.js') }}"></script>
     </body>
 </html>
